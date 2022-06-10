ba29f34440067a07bc108e7e293bcfb442d47559
Marwane BOUABDELLAH <manbou92@hotmail.fr>
2022-06-10 12:28:08 +0200

    ajout des tutoriels sur la première map

--- maps/starter/script.js
+++ maps/starter/script.js
@@ -4,11 +4,28 @@ let currentPopup = undefined;
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+
+/* Tutoriel 1 */
+WA.room.onEnterZone('tuto-1', () => {
+    currentPopup =  WA.ui.openPopup("tuto1Popup","Bienvenue sur notre map !",[]);
 })
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onLeaveZone('tuto-1', closePopUp)
+
+/* Tutoriel 2 */
+WA.room.onEnterZone('tuto-2', () => {
+    currentPopup =  WA.ui.openPopup("tuto2Popup","Un espace créatif pour coder " +
+        "ensemble sur VSCode est née.",[]);
+})
+
+WA.room.onLeaveZone('tuto-2', closePopUp)
+
+/* Tutoriel 3 */
+WA.room.onEnterZone('tuto-3', () => {
+    currentPopup =  WA.ui.openPopup("tuto3Popup","Il suffit de se placer dans un pupitre pour créer l'environnement collaboratif",[]);
+})
+
+WA.room.onLeaveZone('tuto-3', closePopUp)
 /*
 const vsCode = await WA.ui.website.open({
     url: "https://vscode.dev",
